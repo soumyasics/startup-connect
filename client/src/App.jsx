d6ac980422c16b980b8192ac9f57bd255208f2ab
ajeena <ajeena.sics@gmail.com>
2024-05-27 15:01:34 +0530

    startup connect started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import LandingPage from "./Components/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1> Home page</h1>} />
+        <Route path="/" element={<LandingPage/>} />
         
         {/* This routes should be last  */}
         <Route path="/*" element={<h1>404 page</h1>} />
