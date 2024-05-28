3fa02384626c8f8418fcb7651a26f52ce54f10aa
sheik2313 <sheikshahul6@gmail.com>
2024-05-28 23:10:31 +0530

    image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPageTopNav from "./LandingPage/LandingPageTopNav";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPageTopNav/>}/>
+      <Route path="/footer" element={<Footer/>}/>
     </Routes>
     </BrowserRouter>
   );
