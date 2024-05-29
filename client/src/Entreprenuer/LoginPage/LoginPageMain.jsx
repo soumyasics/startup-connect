72b80a12790a30765efe88ef7dc9cc06527d7d67
bijo <bijoksanoai@gmail.com>
2024-05-29 14:10:37 +0530

    bijo1

--- client/src/Entreprenuer/LoginPage/LoginPageMain.jsx
+++ client/src/Entreprenuer/LoginPage/LoginPageMain.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./LoginPageMain.css"
-
+import Loginimage from '../../assets/Rectangle 10.png'
 function LoginPageMain() {
   return (
     <div>
@@ -12,7 +12,14 @@ function LoginPageMain() {
     </div>
     <div className='ent-loginpage-line' Default></div>
     <div className='ent-loginpage-img_div'>
-      <img className='ent-loginpage-img'/>
+      <img className='ent-loginpage-img' src={Loginimage}/>
+    </div>
+    <div className='ent-loginpage-loginform'>
+      <label class='ent-loginpage-email-label'>Your Email</label>
+      <input type="email" className='ent-loginpage-email'/>
+      <label class='ent-loginpage-password-label'>Password</label>
+      <input type="password" className='ent-loginpage-password'/>
+      <button className='ent-loginpage-login'>Log In</button>
     </div>
     </div>
   )
