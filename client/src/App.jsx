0076148f7788d9b96b7bdfdddf8b76df2eaad80d
bijo <bijoksanoai@gmail.com>
2024-05-29 12:39:40 +0530

    image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPageTopNav from "./LandingPage/LandingPageTopNav";
+import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPageTopNav/>}/>
+      <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
     </Routes>
     </BrowserRouter>
   );
---
+++ client/src/Entreprenuer/LoginPage/LoginPageMain.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./LoginPageMain.css"
+
+function LoginPageMain() {
+  return (
+    <div>
+    <div className="ent-loginpage-loginhere">
+        <p>Login Here</p>
+    </div>
+    <div className="ent-loginpage-content">
+      <p>Your Journey to Success Starts Here</p>
+    </div>
+    <div className='ent-loginpage-line' Default></div>
+    <div className='ent-loginpage-img_div'>
+      <img className='ent-loginpage-img'/>
+    </div>
+    </div>
+  )
+}
+
+export default LoginPageMain
