c8310b27893ba01ba77f017aeeb7aee2e98d7026
bijo <bijoksanoai@gmail.com>
2024-05-30 12:40:01 +0530

    loginfunctionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,19 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPageTopNav from "./LandingPage/LandingPageTopNav";
-// import Footer from "./components/Footer/Footer";
-import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain";
 import Footer from "./components/Footer/Footer";
 import ForgotPassMain from "./Entreprenuer/ForgotPass/ForgotPassMain";
+import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain";
+
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPageTopNav/>}/>
-
       <Route path="/footer" element={<Footer/>}/>
       <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
-   <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
-   <Route path="/ent-forgotpassmain" element={<ForgotPassMain/>}/>
-
-      <Route path="/footer" element={<Footer/>}/>
+      <Route path="/ent-forgotpassmain" element={<ForgotPassMain/>}/>
 
     </Routes>
     </BrowserRouter>
