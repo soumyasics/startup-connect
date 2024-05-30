9482d6688653a46f5a9b8cf23bb2d069c98d2e19
bijo <bijoksanoai@gmail.com>
2024-05-30 10:31:09 +0530

    forgotpass

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPageTopNav from "./LandingPage/LandingPageTopNav";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain";
+import Footer from "./components/Footer/Footer";
+import ForgotPassMain from "./Entreprenuer/ForgotPass/ForgotPassMain";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPageTopNav/>}/>
+
       <Route path="/footer" element={<Footer/>}/>
       <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
+   <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
+   <Route path="/ent-forgotpassmain" element={<ForgotPassMain/>}/>
+
+      <Route path="/footer" element={<Footer/>}/>
+
     </Routes>
     </BrowserRouter>
   );
