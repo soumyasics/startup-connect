bcdb5e686021b28910d0a560fed475faa598d4d4
Anand R P <anandrp.sics@gmail.com>
2024-05-31 17:36:39 +0530

    class changed to className

--- client/src/Entreprenuer/ForgotPass/ForgotPassMain.jsx
+++ client/src/Entreprenuer/ForgotPass/ForgotPassMain.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import './ForgotPassMain.css'
-import Forgotpasswordimage from '../../assets/ForgotPassword.png'
+import React from "react";
+import "./ForgotPassMain.css";
+import Forgotpasswordimage from "../../assets/ForgotPassword.png";
 
 function ForgotPassMain() {
   return (
-    <div className='container'>
+    <div className="container">
       <div className="ent-forgotpass-forgotpassword">
         <p>Forgot Password</p>
       </div>
       <div className="ent-forgotpass-content">
         <p>Recover Your Account</p>
       </div>
-      <div className='ent-forgotpass-line'></div>
-      <div className='ent-forgotpass-img_div'>
-        <img className='ent-forgotpass-img' src={Forgotpasswordimage}/>
+      <div className="ent-forgotpass-line"></div>
+      <div className="ent-forgotpass-img_div">
+        <img className="ent-forgotpass-img" src={Forgotpasswordimage} />
       </div>
-      <form className='ent-forgotpass-forgotpassform'>
-        <label class='ent-forgotpass-email-label'>Your Email</label>
-        <input type="email" className='ent-forgotpass-email'/>
-        <label class='ent-forgotpass-newpassword-label'>New Password</label>
-        <input type="password" className='ent-forgotpass-newpassword'/>
-        <label class='ent-forgotpass-confirmpassword-label'>Confirm Password</label>
-        <input type="password" className='ent-forgotpass-confirmpassword'/>
-        <button className='ent-forgotpass-resetbtn'>Reset</button>
-        <button className='ent-forgotpass-cancelbtn'>Cancel</button>
+      <form className="ent-forgotpass-forgotpassform">
+        <label className="ent-forgotpass-email-label">Your Email</label>
+        <input type="email" className="ent-forgotpass-email" />
+        <label className="ent-forgotpass-newpassword-label">New Password</label>
+        <input type="password" className="ent-forgotpass-newpassword" />
+        <label className="ent-forgotpass-confirmpassword-label">
+          Confirm Password
+        </label>
+        <input type="password" className="ent-forgotpass-confirmpassword" />
+        <button className="ent-forgotpass-resetbtn">Reset</button>
+        <button className="ent-forgotpass-cancelbtn">Cancel</button>
       </form>
-      </div>
-    
-  )
+    </div>
+  );
 }
 
-export default ForgotPassMain
+export default ForgotPassMain;
