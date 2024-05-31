78b9853c364490f931d6bec576d22b4d37bdecbc
Anand R P <anandrp.sics@gmail.com>
2024-05-31 13:24:27 +0530

    axios instance config completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,24 +6,23 @@ import LandingPageTopNav from "./LandingPage/LandingPageTopNav";
 import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain";
 import ForgotPassMain from "./Entreprenuer/ForgotPass/ForgotPassMain";
 
-
-
 import Entsignup from "./Entreprenuer/Signup/Entsignup";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={[<LandingPageTopNav/>,<Footer/>]}/>
-   <Route path="/entsignup" element={<Entsignup/>}/>
-   <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
-   <Route path="/ent-forgotpassmain" element={<ForgotPassMain/>}/>
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={[<LandingPageTopNav />, <Footer />]} />
+        <Route path="/entrepreneur/signup" element={<Entsignup />} />
+        <Route path="/entrepreneur/login" element={<LoginPageMain />} />
+        <Route
+          path="/entrepreneur/fogot-password"
+          element={<ForgotPassMain />}
+        />
 
-
-
-    </Routes>
+        <Route path="/*" element={<h1> 404 Page Not Found</h1>} />
+      </Routes>
     </BrowserRouter>
   );
 }
