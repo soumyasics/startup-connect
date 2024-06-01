0cbd6765606507b840d83f53971a7d5896967077
bijo <bijoksanoai@gmail.com>
2024-06-01 16:05:28 +0530

    startup_investor_reg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,17 +10,23 @@ import ForgotPassMain from "./Entreprenuer/ForgotPass/ForgotPassMain";
 import Entsignup from "./Entreprenuer/Signup/Entsignup";
 import Footer from "./components/Footer/Footer";
 
+{/* Invester */}
+
+import InvesterRegister from "./components/Invester/InvesterRegister";
+
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      {/* Entreprenuer */}
       <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
       <Route path="/ent-forgotpassmain" element={<ForgotPassMain/>}/>
-    <Route path="/" element={[<LandingPageTopNav/>,<Footer/>]}/>
-   <Route path="/entsignup" element={<Entsignup/>}/>
-  
+      <Route path="/" element={[<LandingPageTopNav/>,<Footer/>]}/>
+      <Route path="/entsignup" element={<Entsignup/>}/>
 
+      {/* Invester */}
+      <Route path="inv-register" element={<InvesterRegister/>}/>
 
 
     </Routes>
