471fce668d1f0cdceaf142286ce84f82e588a1df
ajeena <ajeena.sics@gmail.com>
2024-06-03 11:00:55 +0530

    corrected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPageTopNav from "./LandingPage/LandingPageTopNav";
 
-import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain";
-import ForgotPassMain from "./Entreprenuer/ForgotPass/ForgotPassMain";
+import LoginPageMain from "./Entreprenuer/LoginPage/LoginPageMain"
+import ForgotPassMain  from "./Entreprenuer/ForgotPass/ForgotPassMain"
+import Footer from "./Components/Footer/Footer"
 
+import Entsignup from "./Entreprenuer/Signup/Entsignup"
 
-import Entsignup from "./Entreprenuer/Signup/Entsignup";
+{
+  /* Invester */
+}
 
-{/* Invester */}
-
-import InvesterRegister from "./components/Invester/InvesterRegister";
-
+import InvesterRegister from "./Components/Invester/InvesterRegister"
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      {/* Entreprenuer */}
-      <Route path="/ent-loginpagemain" element={<LoginPageMain/>} />
-      <Route path="/ent-forgotpassmain" element={<ForgotPassMain/>}/>
-      <Route path="/" element={[<LandingPageTopNav/>,<Footer/>]}/>
-      <Route path="/entsignup" element={<Entsignup/>}/>
+      <Routes>
+        {/* Entreprenuer */}
+        <Route path="/ent-loginpagemain" element={<LoginPageMain />} />
+        <Route path="/ent-forgotpassmain" element={<ForgotPassMain />} />
+        <Route path="/" element={[<LandingPageTopNav />, <Footer />]} />
+        <Route path="/entsignup" element={<Entsignup />} />
 
-      {/* Invester */}
-      <Route path="inv-register" element={<InvesterRegister/>}/>
-
-
-    </Routes>
+        {/* Invester */}
+        <Route path="inv-register" element={<InvesterRegister />} />
+      </Routes>
     </BrowserRouter>
   );
 }
