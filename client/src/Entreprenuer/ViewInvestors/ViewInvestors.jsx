c7cc15ae93dbff9bee2c8dcaedfb3e8fa45c8052
bijo <bijoksanoai@gmail.com>
2024-06-17 15:45:55 +0530

    bijo_startup_17.6.24_2

--- client/src/Entreprenuer/ViewInvestors/ViewInvestors.jsx
+++ client/src/Entreprenuer/ViewInvestors/ViewInvestors.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ViewInvestors.css'
 import { CommonNavbar } from '../../components/commonNavbar/commonNavbar'
 import HomepageNavbar from '../../components/commonNavbar/HomepageNavbar'
 import Investor1 from '../../assets/Investor1.png'
 import Footer from '../../components/Footer/Footer'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../BaseAPIs/AxiosInstance'
+import { imageUrl } from '../../ImageAPIs/Image_Urls'
+
 
 function InvestorsView() {
+  const navigate=useNavigate();
+
+  const [investorData, setInvestordata]= useState({});
+
+  useEffect(()=>{
+    if(localStorage.getItem("Enterprenuertoken")== null && localStorage.getItem("Enterprenuer") == null ){
+      navigate("/");
+    }
+  },[navigate]);
+
+  useEffect(()=>{
+    axiosInstance.post('/viewInvestors')
+    .then((res)=>{
+      console.log(res,"res");
+      if(res.status === 200){
+        setInvestordata(res.data.data)
+      }
+    })
+    .catch((err)=>{
+      toast.error("Failed to fetch user details")
+  });
+  },[])
+
+ 
+
+
+
   return (
     <>
     <CommonNavbar/>
@@ -17,83 +48,33 @@ function InvestorsView() {
             <div className="  mb-5  ent_invview_hr_line "></div>
         </div>
         <div class="row row-cols-1 row-cols-md-4 g-4">
+          {console.log("datas",investorData)}
+         {
+        (investorData.length)>0?((investorData).map((data) => {
+          return(
+            
           <div class="col">
             <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
+              <img src={`${imageUrl}/${data.profile.filename}`} class="ent_invview_profile_pic" alt="..."/>
               <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
+                <h5 class="ent_invview_fname">{data.name}</h5>
                 <h3 className='ent_invview_name'>INVESTOR</h3>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="ent_invview_profile">
-              <img src={Investor1} class="ent_invview_profile_pic" alt="..."/>
-              <div class="">
-                <h5 class="ent_invview_fname">Full Name</h5>
-                <h3 className='ent_invview_name'>INVESTOR</h3>
-              </div>
-            </div>
-          </div>
+            
+          )
+        })):(
+        
+          <h1>ghghh</h1>
+        )
+        } 
+          
         </div>
       </div>
       <Footer/>
     </>
   )
-}
 
+}
 export default InvestorsView
