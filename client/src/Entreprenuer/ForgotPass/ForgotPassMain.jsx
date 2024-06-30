f33fc37352237fb3121b587f05e60bf83cb71d6d
ajeena <ajeena.sics@gmail.com>
2024-06-30 11:42:57 +0530

    done

--- client/src/Entreprenuer/ForgotPass/ForgotPassMain.jsx
+++ client/src/Entreprenuer/ForgotPass/ForgotPassMain.jsx
@@ -3,10 +3,8 @@ import "./ForgotPassMain.css";
 import Forgotpasswordimage from "../../assets/ForgotPassword.png";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../BaseAPIs/AxiosInstance";
-import { Link } from "react-router-dom";
 import Navbar_2 from "../../components/commonNavbar/Navbar_2";
 import Footer from "../../components/Footer/Footer";
-
 
 function ForgotPassMain() {
   const [data, setData] = useState({
@@ -31,40 +29,40 @@ function ForgotPassMain() {
     });
     setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
- const HandleCancel=()=>{
-  Navigate("/entrepreneur/login")
- }
+
+  const HandleCancel = () => {
+    Navigate("/entrepreneur/login");
+  };
+
   const HandleClick = (e) => {
     e.preventDefault();
-    console.log(data, "l");
-    let errors = {};
+    let validationErrors = {};
     let formValid = true;
 
     if (!data.email.trim()) {
       formValid = false;
-      errors.email = "Email is required";
+      validationErrors.email = "Email is required";
     }
-
-    
 
     const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[A-Z]).{6,}$/;
     if (!data.password.trim()) {
       formValid = false;
-      errors.password = "Password is required";
+      validationErrors.password = "Password is required";
     } else if (!passwordRegex.test(data.password)) {
-      errors.password =
+      formValid = false;
+      validationErrors.password =
         "Password must contain at least one number, one special character, and one capital letter";
     }
 
     if (!data.confirmpassword.trim()) {
       formValid = false;
-      errors.confirmpassword = "Confirm Password is required";
+      validationErrors.confirmpassword = "Confirm Password is required";
     } else if (data.confirmpassword !== data.password) {
       formValid = false;
-      errors.confirmpassword = "Passwords do not match";
+      validationErrors.confirmpassword = "Passwords do not match";
     }
 
-    setErrors(errors);
+    setErrors(validationErrors);
 
     if (formValid) {
       axiosInstance
@@ -83,69 +81,77 @@ function ForgotPassMain() {
 
   return (
     <>
-    <Navbar_2/>
+      <Navbar_2 />
       <div className="container ">
-      <div className="text-center ">
-            <h4 className="  mt-3  ent_forgot_mainheading">FORGOT PASSWORD</h4>
-            <h3 className="ent_forgot_sub_h3">Recover Your Account </h3>
-            <div className="  mb-5  ent_forgot_hr_line "></div>
-
+        <div className="text-center ">
+          <h4 className="mt-3 ent_forgot_mainheading">FORGOT PASSWORD</h4>
+          <h3 className="ent_forgot_sub_h3">Recover Your Account </h3>
+          <div className="mb-5 ent_forgot_hr_line"></div>
         </div>
-        <div className='row '>
-      <div className="col ent_forgot_img_div">
-        <img className="ent_forgot_img mb-4" src={Forgotpasswordimage} />
-      </div>
-      <div className='col mt-5 px-5'>
-      <form className="ent_forgot_form">
-        <label className="ent_forgot_email_label">Your Email</label>
-        <input
-          type="email"
-          name="email"
-          onChange={handleInputChange}
-          className="ent_forgot_email"/>
-          {errors.email && (
-            <div className="text-danger errortext">{errors.email}</div>
-          )}
-        <label className="ent_forgot_password_label">Password</label>
-        <input
-          type="password"
-          name="password"
-          onChange={handleInputChange}
-          className="ent_forgot_password"
-        />
-        {errors.password && (
-            <div className="text-danger errortext">{errors.password}</div>
-        )}
-        <label className="ent_forgot_cpassword_label">Confirm Password</label>
-        <input
-          type="password"
-          name="confirmpassword"
-          onChange={handleInputChange}
-          className="ent_forgot_cpassword"
-        />
-        {errors.confirmpassword && (
-            <div className="text-danger errortext">
-              {errors.confirmpassword}
-            </div>
-          )}
         <div className="row">
-          <div className="col">
-        <button className="ent_forgot_resetbtn" type="submit" onClick={HandleClick} >
-          Reset Password
-        </button>
+          <div className="col ent_forgot_img_div">
+            <img className="ent_forgot_img mb-4" src={Forgotpasswordimage} />
+          </div>
+          <div className="col mt-5 px-5">
+            <form className="ent_forgot_form">
+              <label className="ent_forgot_email_label">Your Email</label>
+              <input
+                type="email"
+                name="email"
+                onChange={handleInputChange}
+                className="ent_forgot_email"
+              />
+              {errors.email && (
+                <div className="text-danger errortext">{errors.email}</div>
+              )}
+              <label className="ent_forgot_password_label">Password</label>
+              <input
+                type="password"
+                name="password"
+                onChange={handleInputChange}
+                className="ent_forgot_password"
+              />
+              {errors.password && (
+                <div className="text-danger errortext">{errors.password}</div>
+              )}
+              <label className="ent_forgot_cpassword_label">Confirm Password</label>
+              <input
+                type="password"
+                name="confirmpassword"
+                onChange={handleInputChange}
+                className="ent_forgot_cpassword"
+              />
+              {errors.confirmpassword && (
+                <div className="text-danger errortext">
+                  {errors.confirmpassword}
+                </div>
+              )}
+              <div className="row">
+                <div className="col">
+                  <button
+                    className="ent_forgot_resetbtn"
+                    type="submit"
+                    onClick={HandleClick}
+                  >
+                    Reset Password
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    className="ent_forgot_canclebtn"
+                    onClick={HandleCancel}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="col">
-        <button className="ent_forgot_canclebtn" onClick={HandleCancel}  >
-          Cancel
-        </button>
-        </div>
-        </div>
-      </form>
       </div>
-      </div>
-      </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
+
 export default ForgotPassMain;
--- client/src/components/Invester/ViewEntrepreneurReqList/ViewEntreprenrueReqList.jsx
+++ client/src/components/Invester/ViewEntrepreneurReqList/ViewEntreprenrueReqList.jsx
@@ -1,66 +1,120 @@
-import React from 'react'
-import { CommonNavbar } from '../../commonNavbar/commonNavbar'
-import Footer_2 from '../../Footer/Footer_2'
-import InvestorNav from '../InvestorNav/InvestorNav'
+import React, { useEffect, useState } from "react";
+import { CommonNavbar } from "../../commonNavbar/commonNavbar";
+import Footer_2 from "../../Footer/Footer_2";
+import InvestorNav from "../InvestorNav/InvestorNav";
 import eye from "../../../assets/carbon_view-filled.png";
-
+import axiosInstance from "../../../BaseAPIs/AxiosInstance";
+import { useNavigate } from "react-router-dom";
 function ViewEntreprenrueReqList() {
+
+  const [entData, setentData] = useState([]);
+const navigate=useNavigate()
+
+  useEffect(() => {
+    axiosInstance
+      .post("/viewInvestorReqByInvId/" + localStorage.getItem("Investor"))
+      .then((res) => {
+        console.log(res, "res");
+        if (res.status === 200) {
+          setentData(res.data.data);
+        }
+      })
+      .catch((err) => {
+        toast.error("Failed to fetch user details");
+      });
+  }, []);
+
+  const navigateToInvestorView=()=>{
+    navigate("/investor/entrepreneur_req")
+  }
+
 
   return (
     <>
-        <CommonNavbar/>
-        <InvestorNav/>
-        <div className='container'>
-    <div className="text-center ">
+      <CommonNavbar />
+      <InvestorNav />
+      <div className="container">
+        <div className="text-center ">
           <h4 className="  mt-3  inv_mainheading">View All</h4>
           <h3 className="inv_sub_h3">Entrepreneur Requests</h3>
           <div className="  mb-5  inv_hr_line "></div>
-    </div>
-    <table className="table">
-      
-  <thead >
-    <tr  >
-      <th  style={{backgroundColor:"rgba(140, 220, 249, 1)"}} scope="col">Name</th>
-      <th  style={{backgroundColor:"rgba(140, 220, 249, 1)"}} scope="col">E-Mail ID</th>
-      <th  style={{backgroundColor:"rgba(140, 220, 249, 1)"}} scope="col">Industry Sector</th>
-      <th  style={{backgroundColor:"rgba(140, 220, 249, 1)"}} scope="col">Contact No</th>
-      <th  style={{backgroundColor:"rgba(140, 220, 249, 1)"}} scope="col">Corporate Identification Number</th>
-      <th  style={{backgroundColor:"rgba(140, 220, 249, 1)"}} scope="col">Action</th>
-
-    </tr>
-  </thead>
-  <tbody>
-  {/* {
-        (entData.length)>0?((entData).map((data) => {
-          return(
-       */}
-    <tr>
-      <th scope="row">
-      {/* <img src={`${imageUrl}/${data.profile.filename}`}  */}
-      {/* class="invviewadmin_profile_pic" alt="..."/> */}
-      name</th>
-      <td>email</td>
-      <td>industry_sector</td>
-      <td>contact</td>
-      <td>corporate_id_no</td>
-      <td style={{color:"rgba(52, 133, 208, 1)"}} ><img src={eye}></img> <a href="" onClick={()=>navigateToInvestorView(data._id)}>View Details</a></td>
-
-    </tr>
-   {/* )
-  })):(
-  
-    <h1>No Records</h1>
-  )
-  }  */}
-  </tbody>
-
-  
-</table>
-  
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th
+                style={{ backgroundColor: "rgba(140, 220, 249, 1)" }}
+                scope="col"
+              >
+                Name
+              </th>
+              <th
+                style={{ backgroundColor: "rgba(140, 220, 249, 1)" }}
+                scope="col"
+              >
+                E-Mail ID
+              </th>
+              <th
+                style={{ backgroundColor: "rgba(140, 220, 249, 1)" }}
+                scope="col"
+              >
+                Industry Sector
+              </th>
+              <th
+                style={{ backgroundColor: "rgba(140, 220, 249, 1)" }}
+                scope="col"
+              >
+                Contact No
+              </th>
+              <th
+                style={{ backgroundColor: "rgba(140, 220, 249, 1)" }}
+                scope="col"
+              >
+                Corporate Identification Number
+              </th>
+              <th
+                style={{ backgroundColor: "rgba(140, 220, 249, 1)" }}
+                scope="col"
+              >
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {entData.length > 0 ? (
+              entData.map((data) => {
+                return (
+                  <tr>
+                    <th scope="row">
+                      {/* <img src={`${imageUrl}/${data.profile.filename}`}  */}
+                      {/* class="invviewadmin_profile_pic" alt="..."/> */}
+                      {data.entId.fname}
+                    </th>
+                    <td>{data.entId.email}</td>
+                    <td>{data.entId.industry_sector}</td>
+                    <td>{data.entId.contact}</td>
+                    <td>{data.entId.corporate_id_no}</td>
+                    <td style={{ color: "rgba(52, 133, 208, 1)" }}>
+                      <img src={eye}></img>{" "}
+                      <a
+                        href=""
+                        onClick={() => navigateToInvestorView(data._id)}
+                      >
+                        View Details
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <h1>No Records</h1>
+            )}
+          </tbody>
+        </table>
       </div>
-        <Footer_2/>
+      <Footer_2 />
     </>
-  )
+  );
 }
 
-export default ViewEntreprenrueReqList
+export default ViewEntreprenrueReqList;
