1230923319fb9f39d1b1229dc87def5f2601fe42
ajeena <ajeena.sics@gmail.com>
2024-07-01 13:04:24 +0530

    done

--- server/routes.js
+++ server/routes.js
@@ -69,7 +69,7 @@ router.post('/forgotPasswordMentors',mentors.forgotPassword)
 
 
 router.post('/mentorAddBlog/:id',mentors.uploadblog,mentors.mentorAddBlog)
-router.post('/mentorViewBlog',mentors.viewBlogByMentorId)
+router.post('/mentorViewBlog/:id',mentors.viewBlogByMentorId)
 router.post('/mentorViewBlogById/:id',mentors.mentorViewBlogById)
 router.post('/mentorUpdateBlog/:id',mentors.uploadblog,mentors.mentorUpdateBlog)
 router.post('/mentorRemoveBlog/:id',mentors.mentorRemoveBlog)
