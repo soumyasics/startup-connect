515a4f2d0e4984d5e03f798205d58ecb6196b6cd
Ajeenasics <ajeena.sics@gmail.com>
2024-07-27 16:47:45 +0530

    latest

--- client/src/BaseAPIs/AxiosInstance.js
+++ client/src/BaseAPIs/AxiosInstance.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  // baseURL: "http://hybrid.srishticampus.in/strartup_api/",
+  baseURL: "http://hybrid.srishticampus.in/strartup_api/",
 
-  baseURL: 'http://localhost:4040/strartup_api/', 
+  // baseURL: 'http://localhost:4040/strartup_api/', 
   
 
   headers: {
